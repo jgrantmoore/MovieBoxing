7d31af6d3569ebff450825151f7845789ba31766
Grant Moore <jgrantmoore17@gmail.com>
2026-04-08 17:19:29 -0400

    Google Auth Env Changes

--- movie-boxing/auth.ts
+++ movie-boxing/auth.ts
@@ -28,8 +28,8 @@ declare module "next-auth/jwt" {
 export const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
-      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET!,
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
     CredentialsProvider({
       name: "Credentials",
